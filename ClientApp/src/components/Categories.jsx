46153dce6bd771e9d2bf1ec509e26bc78b086fa6
Charan Pasham <cpasham@bloominsuranceagency.com>
2021-01-09 20:04:12 -0500

    update npm packages

--- ClientApp/src/components/Categories.jsx
+++ ClientApp/src/components/Categories.jsx
@@ -1,42 +1,37 @@
-import React, { Component } from 'react';
-import authService from './api-authorization/AuthorizeService';
+import React, { Component } from "react";
+import authService from "./api-authorization/AuthorizeService";
 
 export class Categories extends Component {
   constructor(props) {
-      super(props);
-      this.state = { categories: null, userId: null };
+    super(props);
+    this.state = { categories: null, userId: null };
   }
 
+  componentDidMount = () => {
+    this.populateCategories();
+  };
 
- componentDidMount = () => {
-     this.populateCategories();
- }
-
-    render() {
-        let { categories, userId } = this.state;
-      let renderCategories = categories? categories.map(cat => (
-          <div>{cat.quizCategoryType}</div>
-      )): null;
+  render() {
+    let { categories, userId } = this.state;
+    let renderCategories = categories
+      ? categories.map((cat) => <div>{cat.quizCategoryType}</div>)
+      : null;
     return (
       <div>
         <h1>Categories</h1>
-            {renderCategories}
-            {userId}
+        {renderCategories}
+        {userId}
       </div>
     );
   }
 
-    async populateCategories() {
-        const { sub } = await authService.getUser();
-        const response = await fetch('/api/quiz/category',
-            {
-                method: 'GET',
-                'Content-Type': 'application/json',
-            });
-        const data = await response.json();
-        this.setState({ categories: data, userId: sub });
-
-//            .then(this.throwNonSuccessStatus)
-//            .then(response => response.json());
-    }
+  async populateCategories() {
+    const { sub } = await authService.getUser();
+    const response = await fetch("/api/quiz/category", {
+      method: "GET",
+      "Content-Type": "application/json",
+    });
+    const data = await response.json();
+    this.setState({ categories: data, userId: sub });
+  }
 }
